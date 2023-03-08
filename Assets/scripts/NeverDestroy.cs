64d5be0682624afcae6c359b3a26cb1e6a104340
Damian Pieczykolan / Art3usz <Damian.Pieczykolan@10g.pl>
2023-03-09 00:10:13 +0100

    modified:   Assets/scripts/NeverDestroy.cs

--- Assets/scripts/NeverDestroy.cs
+++ Assets/scripts/NeverDestroy.cs
@@ -1,41 +1,34 @@
-﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class NeverDestroy : MonoBehaviour
-{
+public class NeverDestroy : MonoBehaviour {
     int point;
     int life;
     //komponet 
     AudioSource aS;
     // Use this for initialization
-    void Start()
-    {
+    void Start () {
         //zadbanie o to by obiekt nie byl niszczony podczas ladowania nowej sceny
-        DontDestroyOnLoad(gameObject);
-        aS = gameObject.GetComponent<AudioSource>();
-        SceneManager.LoadScene("Menu");
+        DontDestroyOnLoad (gameObject);
+        aS = gameObject.GetComponent<AudioSource> ();
+        SceneManager.LoadScene ("Menu");
     }
-    public int getLifes()
-    {
+    public int getLifes () {
         return life;
     }
 
-      public void setLifes(int life)
-    {
-       this.life=life;
+    public void setLifes (int life) {
+        this.life = life;
     }
-    public int getPoint()
-    {
+    public int getPoint () {
         return point;
     }
-    public void setPoint(int points)
-    {
+    public void setPoint (int points) {
         this.point = points;
     }
-    public void Mute()
-    {
+    public void Mute () {
         aS.mute = !aS.mute;
     }
 
